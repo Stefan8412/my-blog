fa143847e75269e11252e0c093e8deace21ba775
Stefan Hancar <stefanhancar@MacBook-Air-uzivatela-Stefan.local>
2020-06-16 21:21:54 +0200

    first commit

---
+++ src/build/precache-manifest.76b9f025983db9b2d434b119a8c02a74.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcfd81839614f98c39a85d8a762a50c4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f22b57ac1538a98af5d9",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "577cf49ca3f544d57bfd",
+    "url": "/static/js/2.95a56359.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.95a56359.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f22b57ac1538a98af5d9",
+    "url": "/static/js/main.36f4d413.chunk.js"
+  },
+  {
+    "revision": "36adbc7179ce49d2ac8a",
+    "url": "/static/js/runtime-main.79e3e27f.js"
+  }
+]);
